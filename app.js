e05269bfb2ee2ac37db21f3731fab8a3213045ac
medeirosanderson <contato.harmonicamente@gmail.com>
2024-11-05 14:06:08 -0300

    inserindo codigo de verificação de Nivel do Heroi

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("helo world");
+let nomeHeroi = "Carcará"
+let nivelHeroi = 5400   
+
+switch (true) {
+    case nivelHeroi < 1000:
+        console.log(`O herói ${nomeHeroi} está no nível Ferro`);
+        break;
+    case nivelHeroi >= 1000 && nivelHeroi <= 2000:
+        console.log(`O herói ${nomeHeroi} está no nível Bronze`);
+        break;
+    case nivelHeroi >= 2001 && nivelHeroi <= 5000:
+        console.log(`O herói ${nomeHeroi} está no nível Prata`);
+        break;
+    case nivelHeroi >= 5001 && nivelHeroi <= 7000:
+        console.log(`O herói ${nomeHeroi} está no nível Ouro`);
+        break;
+    case nivelHeroi >= 7001 && nivelHeroi <= 8000:
+        console.log(`O herói ${nomeHeroi} está no nível Platina`);
+        break;
+    case nivelHeroi >= 8001 && nivelHeroi <= 9000:
+        console.log(`O herói ${nomeHeroi} está no nível Ascendente`);
+        break;
+    case nivelHeroi >= 9001 && nivelHeroi <= 10000:
+        console.log(`O herói ${nomeHeroi} está no nível Imortal`);
+        break;
+    case nivelHeroi >= 10001:
+        console.log(`O herói ${nomeHeroi} está no nível Radiante`);
+        break;              
+}
